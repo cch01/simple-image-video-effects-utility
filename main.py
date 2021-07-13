c8390795255bc200b673d71f6a673110304e4fb2
Terry Chan <cch01@protonmail.com>
2021-07-13 21:39:57 +0800

    Basic cli component

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import cv2
 from filterUtils import resizeDisplayingWindow
 from filters import Filters
+from enum import Enum
 
-def renderVideo(path):
-  vid = cv2.VideoCapture(path)
-  fps = round(vid.get(cv2.CAP_PROP_FPS))
+# import argparse
+
+filterMapper = {
+  1: Filters.invertedFilter,
+  2: Filters.sepiaFilter,
+  3: Filters.coolerFilter,
+  4: Filters.warmerFilter,
+  5: Filters.paint,
+  6: Filters.reduceNoise,
+  7: Filters.blurFilter,
+  8: Filters.sharpening,
+  9: Filters.negative
+}
+class SrcType(Enum):
+  VIDEO = 0
+  IMAGE = 1
+
+def renderVideo(vid, filters):
   if not vid.isOpened():
     print('Error on opening video file')
-
+  fps = round(vid.get(cv2.CAP_PROP_FPS))
   while(vid.isOpened()):
     ret, frame = vid.read()
     if ret == True:
@@ -21,8 +37,7 @@ def renderVideo(path):
   vid.release()
   cv2.destroyAllWindows()
 
-def renderPhoto(path):
-  img = cv2.imread(path)
+def renderPhoto(img, filters):
   cv2.imshow('ori_img', resizeDisplayingWindow(img, 480))
 
   cv2.imshow('img', resizeDisplayingWindow(img, 480))
@@ -30,8 +45,52 @@ def renderPhoto(path):
   cv2.destroyAllWindows()
   cv2.imwrite('img.jpg', img)
 
+def openSrcFile():
+  path = input('Image/Video location:\n')
+  if path.endswith('.mp4'):
+    try:
+      vid = cv2.VideoCapture(path)
+      if vid.isOpened():
+        return (vid, SrcType.VIDEO)
+
+    except:
+      print('Please provide correct file source path')
+      return openSrcFile()
+
+  if path.endswith('.jpg') or path.endswith('.png'):
+    img = cv2.imread(path)
+    try:
+      img.shape[0]
+      return (img, SrcType.IMAGE)
+    except:
+      print('Please provide correct file source path')
+      return openSrcFile()
+  
+  print('Please provide correct file source path')
+  return openSrcFile()
+
+
+def askForFilters():
+  filters = input('Select filters to be applied:\n1. Inverted Filter\n2. Sepia Filter\n3. Cooler\n4. Warmer\n5. Paint Effect\n6. Noise Reduction\n7. Blur\n8. Sharpening\n9. negative\n\nAdd "," between filters if multiple filters are desired.\n')
+  try:
+    inputs = [int(f) for f in filters.split(",")]
+    isValidInputs = all(val < 10 and val >= 0 for val in inputs)
+    if not isValidInputs:
+      print('Please provide correct values')
+      return askForFilters()
+    return inputs
+
+  except:
+    print('Please provide correct values')
+    return askForFilters()
+
 if __name__ == '__main__':
-  renderPhoto('portraitTest.jpg')
-  renderVideo('video.mp4')
+  file, srcType = openSrcFile()
+  filtersSelected = askForFilters()
+  if srcType == SrcType.VIDEO:
+    renderVideo(file, filtersSelected)
+  if srcType == SrcType.IMAGE:
+    renderPhoto(file, filtersSelected)
+
   
 # TODO make CLI, LOGIC for saving modified outputs
